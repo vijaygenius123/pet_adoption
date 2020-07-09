8adb9140ed24ee9912cfa047745e656354b33382
Vijayaraghavan Sundararaman <vijaygenius123@gmail.com>
2020-07-09 22:11:18 +0530

    Added Home Page Template

--- adoptions/views.py
+++ adoptions/views.py
@@ -1,9 +1,19 @@
 from django.shortcuts import render
-from django.http import HttpResponse
+from django.http import Http404
 
+from .models import Pet
 # Create your views here.
 def home(request):
-    return HttpResponse('<h1>Home</h1>')
+    pets = Pet.objects.all()
+    return render(request,'home.html',{
+        'pets':pets,
+    })
 
 def pet_detail(request,pet_id):
-    return HttpResponse(f'<h1>Pet Detail {pet_id}</h1>')
+    try:
+        pet = Pet.objects.get(id=pet_id)
+    except Pet.DoesNotExist:
+        raise Http404('Pet not found')
+    return render(request,'pet_detail.html',{
+        'pet':pet,
+    })
